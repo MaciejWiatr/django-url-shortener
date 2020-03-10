8cda92bd65e3dba11aaade1e2a33a0bd9de28737
Dettlaff <maciekwiatr17@gmail.com>
2020-03-10 11:40:04 +0100

    Add basic forms and models tests

---
+++ shorter/tests/test_models.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+from shorter.models import Link
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.Link1 = Link.objects.create(
+            url='https://www.jetbrains.com/',
+        )
+
+    def test_link_is_assigned_code_on_creation(self):
+        self.assertTrue(self.Link1.code)
+
+    def test_link_code_is_unique(self):
+        with self.assertRaises(Exception) as context:
+            Link_uni1 = Link.objects.create(
+                url='https://www.jetbrains.com/',
+                code='testCase'
+            )
+            Link_uni2 = Link.objects.create(
+                url='https://www.jetbrains.com/',
+                code='testCase'
+            )
+        self.assertTrue('UNIQUE' in str(context.exception))
